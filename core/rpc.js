baf4eb410e1855f155601e197f880d487c409a02
CaveyChan <chenjunhuaone@qq.com>
2016-06-16 18:32:37 +0800

    Update rpc.js

--- core/rpc.js
+++ core/rpc.js
@@ -3,18 +3,22 @@
  */
 import conf from './conf.js';
 
-exports.uploadImage = function (uri, key, token, onresp) {
+exports.uploadFile = function (uri, token, formInput) {
+  if (typeof formInput !== 'object') {
+    return false;
+  }
+  
   let formData = new FormData();
-  formData.append('file', {uri: uri, type: 'application/octet-stream', name: key});
-  formData.append('key', key);
-  formData.append('token', token);
-
+  for (let k in formInput) {
+    formData.append(k, formInput.k);
+  }
+  if(!formInput.file) formData.append('file', {uri: uri, type: 'application/octet-stream'});
+  if(!formInput.token) formData.append('token', token);
+  
   let options = {};
   options.body = formData;
   options.method = 'post';
-  return fetch(conf.UP_HOST, options).then((response) => {
-    onresp(response);
-  });
+  return fetch(conf.UP_HOST, options);
 }
 
 //发送管理和fop命令,总之就是不上传文件
